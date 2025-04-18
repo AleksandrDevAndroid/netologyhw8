aa4cc8d0805924b57351e69a7ccff6599fb71eab
AleksandrDevAndroid <aleksperdev@gmail.com>
2025-04-18 13:08:58 +0300

    Delete TaxEarningsMinusSpendings.java

--- TaxEarningsMinusSpendings.java
+++
@@ -1,8 +0,0 @@
-package taxes;
-
-public class TaxEarningsMinusSpendings extends TaxSystem {
-    @Override
-    public int calcTaxFor(int debit, int credit) {
-        return (debit - credit) * 15 / 100 > 0 ? (debit - credit) * 15 / 100 : 0;
-    }
-}
