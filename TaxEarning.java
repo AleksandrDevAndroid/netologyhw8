4ff0a0811e9c1bbddc274cef8d9453d34629aa3f
AleksandrDevAndroid <aleksperdev@gmail.com>
2025-04-18 13:08:36 +0300

    Delete TaxEarning.java

--- TaxEarning.java
+++
@@ -1,8 +0,0 @@
-package taxes;
-
-public class TaxEarning extends TaxSystem {
-    @Override
-    public int calcTaxFor(int debit, int credit) {
-        return debit >= 1000 ? debit * 6 / 100 : 0;
-    }
-}
